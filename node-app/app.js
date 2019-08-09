8d8361b89c75586a05c58048b0fccf6f99b55324
lom <lomkovsky@gmail.com>
2019-08-09 08:33:51 +0300

    Create notes.js and export it function

--- node-app/app.js
+++ node-app/app.js
@@ -1,6 +1,13 @@
-const fs = require('fs');
+const notes = require('./notes.js');
+console.log("Text frome notes.js is = ", notes())
+
+
+
+
+
+// const fs = require('fs');
 // fs.writeFileSync('hello.txt', 'hello!');
-fs.appendFile('hello.txt', ' data to append', (err) => {
-    if (err) throw err;
-    console.log('The "data to append" was appended to file!');
-  });
+// fs.appendFile('hello.txt', ' data to append', (err) => {
+//     if (err) throw err;
+//     console.log('The "data to append" was appended to file!');
+//   });
