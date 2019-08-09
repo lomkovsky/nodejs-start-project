4bf39bf56842623589e265609908befae236805a
lomkovsky <lomkovsky@gmail.com>
2019-08-09 17:28:56 +0300

    Trying to marge

--- node-app/app.js
+++ node-app/app.js
@@ -1,6 +1,8 @@
+const text1 = require('./utils.js');
 const fs = require('fs');
 // fs.writeFileSync('hello.txt', 'hello!');
-fs.appendFile('hello.txt', ' data to append', (err) => {
-    if (err) throw err;
-    console.log('The "data to append" was appended to file!');
-  });
+// fs.appendFile('hello.txt', text1, (err) => {
+//    if (err) throw err;
+//    console.log('The "data to append" was appended to file!');
+//  });
+console.log(text1 (10 ,5));
---
+++ node-app/utils.js
@@ -0,0 +1,5 @@
+const text = ' text frome file utils.js';
+const add = function(a, b){
+    return a + b;
+}
+module.exports = add;
