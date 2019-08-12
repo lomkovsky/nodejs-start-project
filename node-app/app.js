53f6f1fd041f1ebdb93a55709227ca07fe046cc2
lom <lomkovsky@gmail.com>
2019-08-12 10:06:26 +0300

    lesson 16 added yargs

--- node-app/app.js
+++ node-app/app.js
@@ -1,12 +1,33 @@
 const chalk = require('chalk');
 const validator = require('validator');
-const command = process.argv[2];
-const a = 3;
-const b = 6;
-if (command === "add"){
-    console.log(a + b);
-} else if (command === "mult"){
-    console.log(a * b)
- } else {
-     console.log("Chuse 'add' or 'mult'")
- }
+const yargs = require('yargs');
+yargs.command({
+    command: 'add',
+    describe: 'Add a note',
+    handler: function(){
+        console.log('Adding the note')
+    }
+});
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing the note')
+    }
+});
+yargs.command({
+    command: 'list',
+    describe: 'Listing a note',
+    handler: function(){
+        console.log('Listing the note')
+    }
+});
+yargs.command({
+    command: 'read',
+    describe: 'Reading a note',
+    handler: function(){
+        console.log('Reading the note')
+    }
+});
+console.log(yargs.argv);
+
