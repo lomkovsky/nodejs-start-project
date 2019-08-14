bbe21a46782186d3aca1d7fa333fb85bbc6b1c1d
lomkovsky <lomkovsky@gmail.com>
2019-08-14 19:35:01 +0300

    lesson 40 destructuring my functions

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,17 +4,17 @@ const geoCode = require('./utils/geoCode.js');
 const weatherCode = require('./utils/weatherCode.js');
 const name = process.argv[2];
 if (name) {
-    geoCode(name, (error, data) => {
+    geoCode(name, (error, {latitude, longitude, place}) => {
         if (error) {
             return console.log(chalk.red.inverse(error));
         } else {
-            weatherCode(data.latitude, data.longitude, (error, weatherData) => {
+            weatherCode(latitude, longitude, (error, {timezone, summary, temperature}) => {
                 if (error) {
                     return console.log(chalk.red.inverse(error));
                 } else {
-                    console.log(chalk.blue.inverse('Place name is ' + data.place));
-                    console.log(chalk.yellow('Timezome is ' + weatherData.body.timezone));
-                    console.log(chalk.green.inverse('Temperature is ' + weatherData.body.currently.temperature + ' C'));
+                    console.log(chalk.blue.inverse('Place name is ' + place));
+                    console.log(chalk.yellow('Timezome is ' + timezone));
+                    console.log(chalk.green.inverse(summary + ' Temperature is ' + temperature + ' C'));
                 };
             });
         };
--- weather-app/utils/geoCode.js
+++ weather-app/utils/geoCode.js
@@ -4,16 +4,16 @@ const request = require('request');
 
 const geoCode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ address +'.json?access_token=pk.eyJ1IjoibG9ta292c2t5IiwiYSI6ImNqemI1ZHRtazAyOWwzZG1oZWZmZ2g2amEifQ.O2Ubdm4UBoPvtZqudQsmzA&limit=1'
-    request({ url: url, json: true}, (error, response) => {
+    request({ url, json: true}, (error, { body }) => {
         if (error) {
             callback('Unable to connect to geo service!');
-        } else if (response.body.features == 0) {
+        } else if (body.features == 0) {
             callback('Unable to find geo location');
         } else {
             callback(undefined, {
-                place: response.body.features[0].place_name,
-                latitude: response.body.features[0].center[1],
-                longitude: response.body.features[0].center[0]
+                place: body.features[0].place_name,
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0]
             });
         };
     });            
