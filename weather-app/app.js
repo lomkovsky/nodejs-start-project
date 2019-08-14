421471334ac04ac47802c25774013038837e256a
lomkovsky <lomkovsky@gmail.com>
2019-08-14 17:47:53 +0300

    added callback functiion weatheCode() and make a callback chaning

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,25 +1,7 @@
 const request = require('request');
-// const geoUrl ='https://api.mapbox.com/geocoding/v5/mapbox.places/%D0%9C%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0%D1%97%D0%B2,%20%D0%9C%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0%D1%97%D0%B2%D1%81%D1%8C%D0%BA%D0%B0,%20Ukraine.json?access_token=pk.eyJ1IjoibG9ta292c2t5IiwiYSI6ImNqemI1ZHRtazAyOWwzZG1oZWZmZ2g2amEifQ.O2Ubdm4UBoPvtZqudQsmzA&limit=1'
-// request({ url: geoUrl, json: true}, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to geo service!');
-//     } else if (response.body.error){
-//         console.log('Unable to find geo location');
-//     } else {
-//     const data = response;
-//     console.log('Place name is ' + data.body.features[0].place_name);
-//     const weatherUrl = `https://api.darksky.net/forecast/4b7b0ea5f3c454a1b63f90c9014b2f27/${data.body.features[0].center[1]},${data.body.features[0].center[0]}?lang=uk&units=si`;
-//     request({ url: weatherUrl, json: true}, (error, response) => {
-//         if (error) {
-//             console.log('Unable to connect to weather service!');
-//         } else if (response.body.error){
-//             console.log('Unable to find weather location');
-//         } else {
-//          const data = response;
-//          console.log('Timezome is ' + data.body.timezone);
-//          console.log('Temperature is ' + data.body.currently.temperature + ' C');
-//     }});
-// }});
+
+// callback function that give us a latitude, longitude and name of place
+
 const geoCode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ address +'.json?access_token=pk.eyJ1IjoibG9ta292c2t5IiwiYSI6ImNqemI1ZHRtazAyOWwzZG1oZWZmZ2g2amEifQ.O2Ubdm4UBoPvtZqudQsmzA&limit=1'
     request({ url: url, json: true}, (error, response) => {
@@ -28,15 +10,45 @@ const geoCode = (address, callback) => {
         } else if (response.body.features === 0) {
             callback('Unable to find geo location');
         } else {
-            callback(undefined, response);
-        }
+            callback(undefined, {
+                place: response.body.features[0].place_name,
+                latitude: response.body.features[0].center[1],
+                longitude: response.body.features[0].center[0]
+            });
+        };
     });            
 };
-geoCode('mikolayv, Ukraine', (error, data) => {
+geoCode('Micolaiv, Ukraine', (error, data) => {
     //console.log(data.body)
     if (error) {
         console.log(error);
     } else {
-        console.log('Place name is ' + data.body.features[0].place_name);
+        console.log('Place name is ' + data.place);
+        console.log('latitude name is ' + data.latitude);
+        console.log('longitude name is ' + data.longitude);
+        weatherCode(data.latitude, data.longitude, (error, data) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Timezome is ' + data.body.timezone);
+                console.log('Temperature is ' + data.body.currently.temperature + ' C');
+            }; 
+        });
     };
-})
+});
+
+// callback function that give us a temperature and name of time zone
+
+const weatherCode = (latitude, longitude, callback) => {
+    const weatherUrl = 'https://api.darksky.net/forecast/4b7b0ea5f3c454a1b63f90c9014b2f27/' + latitude + ',' + longitude + '?lang=uk&units=si';    
+    request({ url: weatherUrl, json: true}, (error, response) => {
+        if (error) {
+            callback('Unable to connect to weather service!');
+        } else if (response.body.error){
+            callback('Unable to find weather location');
+        } else {
+            callback(undefined, response)
+        };
+    });
+}
+
