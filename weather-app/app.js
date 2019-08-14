a5f31be3537c8e41faad2a1003616ca5d63f031a
lom <lomkovsky@gmail.com>
2019-08-14 16:40:23 +0300

    lesson 36 added geoCode callback function

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,22 +1,42 @@
 const request = require('request');
-const geoUrl ='https://api.mapbox.com/geocoding/v5/mapbox.places/%D0%9C%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0%D1%97%D0%B2,%20%D0%9C%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0%D1%97%D0%B2%D1%81%D1%8C%D0%BA%D0%B0,%20Ukraine.json?access_token=pk.eyJ1IjoibG9ta292c2t5IiwiYSI6ImNqemI1ZHRtazAyOWwzZG1oZWZmZ2g2amEifQ.O2Ubdm4UBoPvtZqudQsmzA&limit=1'
-request({ url: geoUrl, json: true}, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to geo service!');
-    } else if (response.body.error){
-        console.log('Unable to find geo location');
-    } else {
-    const data = response;
-    console.log('Place name is ' + data.body.features[0].place_name);
-    const weatherUrl = `https://api.darksky.net/forecast/4b7b0ea5f3c454a1b63f90c9014b2f27/${data.body.features[0].center[1]},${data.body.features[0].center[0]}?lang=uk&units=si`;
-    request({ url: weatherUrl, json: true}, (error, response) => {
+// const geoUrl ='https://api.mapbox.com/geocoding/v5/mapbox.places/%D0%9C%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0%D1%97%D0%B2,%20%D0%9C%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0%D1%97%D0%B2%D1%81%D1%8C%D0%BA%D0%B0,%20Ukraine.json?access_token=pk.eyJ1IjoibG9ta292c2t5IiwiYSI6ImNqemI1ZHRtazAyOWwzZG1oZWZmZ2g2amEifQ.O2Ubdm4UBoPvtZqudQsmzA&limit=1'
+// request({ url: geoUrl, json: true}, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to geo service!');
+//     } else if (response.body.error){
+//         console.log('Unable to find geo location');
+//     } else {
+//     const data = response;
+//     console.log('Place name is ' + data.body.features[0].place_name);
+//     const weatherUrl = `https://api.darksky.net/forecast/4b7b0ea5f3c454a1b63f90c9014b2f27/${data.body.features[0].center[1]},${data.body.features[0].center[0]}?lang=uk&units=si`;
+//     request({ url: weatherUrl, json: true}, (error, response) => {
+//         if (error) {
+//             console.log('Unable to connect to weather service!');
+//         } else if (response.body.error){
+//             console.log('Unable to find weather location');
+//         } else {
+//          const data = response;
+//          console.log('Timezome is ' + data.body.timezone);
+//          console.log('Temperature is ' + data.body.currently.temperature + ' C');
+//     }});
+// }});
+const geoCode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ address +'.json?access_token=pk.eyJ1IjoibG9ta292c2t5IiwiYSI6ImNqemI1ZHRtazAyOWwzZG1oZWZmZ2g2amEifQ.O2Ubdm4UBoPvtZqudQsmzA&limit=1'
+    request({ url: url, json: true}, (error, response) => {
         if (error) {
-            console.log('Unable to connect to weather service!');
-        } else if (response.body.error){
-            console.log('Unable to find weather location');
+            callback('Unable to connect to geo service!');
+        } else if (response.body.error) {
+            callback('Unable to find geo location');
         } else {
-         const data = response;
-         console.log('Timezome is ' + data.body.timezone);
-         console.log('Temperature is ' + data.body.currently.temperature + ' C');
-    }});
-}})
+            callback(undefined, response);
+        }
+    });            
+};
+geoCode('mikolayv, Ukraine', (error, data) => {
+    //console.log(data.body)
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Place name is ' + data.body.features[0].place_name);
+    };
+})
