9acfbd1f362c4291ac323a3a490372c520ae55c8
lom <lomkovsky@gmail.com>
2019-08-14 13:30:56 +0300

    chenged http request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,7 @@
 const request = require('request');
-const url = 'https://api.darksky.net/forecast/4b7b0ea5f3c454a1b63f90c9014b2f27/37.8267,-122.4233';
-request({ url: url}, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.currently);
+const url = 'https://api.darksky.net/forecast/4b7b0ea5f3c454a1b63f90c9014b2f27/37.8267,-122.4233?lang=uk&units=si';
+request({ url: url, json: true}, (error, response) => {
+    const data = response;
+    console.log(data.body.currently);
 
 })
