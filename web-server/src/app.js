796aa3529de3939956b290adf0aa4a6e0ae7e334
lom <lomkovsky@gmail.com>
2019-08-15 15:49:07 +0300

    lesson 50 added 404 pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -39,6 +39,20 @@ app.get('/help', (req, res) => {
 app.get('/weather', (req, res) => {
     res.send({ name: "Weather page"});
 });
+app.get('/help/*', (req, res) => {
+    res.render('helpArtNotFound', {
+        title: '404',
+        helpfulText: 'like I said - No one will helps you',
+        name: 'lom'
+    });    
+});
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        helpfulText: 'Page not Found. Go away',
+        name: 'lom'    
+    });
+});
 app.listen(3000, () => {
     console.log(calck.green('Server start at port 3000'));
 });
