e57a0df873545c50a6b279fd7d989de9d72fed8c
lom <lomkovsky@gmail.com>
2019-08-15 14:07:04 +0300

    lesson 47 Dynamic pages with hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,9 +1,29 @@
 const path = require('path');
 const express = require('express');
 const calck = require('chalk');
+
 const app = express();
 const publicFolderPath = path.join(__dirname, '../public');
+
+app.set('view engine', 'hbs');
 app.use(express.static(publicFolderPath));
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'lom'    
+    });
+});
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'lom'
+    });
+});
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'No one will helps you'
+    });
+});
 app.get('/weather', (req, res) => {
     res.send({ name: "Weather page"});
 });
