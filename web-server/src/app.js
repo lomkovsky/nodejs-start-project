3f1dd132dc3d7a0af0faee9489ff5f7c33b106f4
lom <lomkovsky@gmail.com>
2019-08-15 09:33:21 +0300

    lesson 43 Install express npm

---
+++ web-server/src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const calck = require('chalk');
+const app = express();
+
+app.get('', (req, res) => {
+    res.send('Hello Express!');
+});
+app.get('/help', (req, res) => {
+    res.send('Help page');
+});
+app.get('/about', (req, res) => {
+    res.send('About page');
+});
+app.get('/weather', (req, res) => {
+    res.send('Weather page')
+});
+app.listen(3000, () => {
+    console.log(calck.green('Server start at port 3000'));
+});
