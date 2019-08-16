5e6991b45b9a3744911d4a177362efefc1b39ba7
lomkovsky <lomkovsky@gmail.com>
2019-08-16 17:47:49 +0300

    chenged lang in weatherCode

--- web-server/src/utils/weatherCode.js
+++ web-server/src/utils/weatherCode.js
@@ -3,7 +3,7 @@ const request = require('request');
 // callback function that give us a temperature and name of time zone
 
 const weatherCode = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/4b7b0ea5f3c454a1b63f90c9014b2f27/' + latitude + ',' + longitude + '?lang=uk&units=si';    
+    const url = 'https://api.darksky.net/forecast/4b7b0ea5f3c454a1b63f90c9014b2f27/' + latitude + ',' + longitude + '?units=si';    
     request({ url, json: true}, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!');
