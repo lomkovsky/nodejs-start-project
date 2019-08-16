41238e123f9711ba6e4ec0d5e635d2b37ae13c93
lom <lomkovsky@gmail.com>
2019-08-16 09:28:56 +0300

    lesson 58 added search  form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,23 @@
-console.log('app.js loaded')
+//alert('app.js loaded');
+
+const weatherForm = document.querySelector('form');
+const search =document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = search.value;
+    console.log(location);
+    const url = 'http://localhost:3000/weather?address=' + location;
+    fetch(url).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error);
+        } else {
+            console.log(data);
+            const temp = data.temperature;
+            console.log(temp);
+        };
+    });
+});
+})
