3d4be601463b6c199caaa949d00c8627479885c1
lomkovsky <lomkovsky@gmail.com>
2019-08-20 20:29:11 +0300

    Lesson 89 Resource created endpoints

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api';
 
 
@@ -8,44 +7,6 @@ mongoose.connect(connectionURL, {
     useCreateIndex: true 
 });
 
-const User = mongoose.model('User', {
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value){
-        if (!validator.isEmail(value)){
-            throw Error('Email is invalid')
-        };    
-    }
-  },  
-  age: {
-    type: Number,
-    default: 0,
-    validate(value){
-        if (value < 0){
-            throw Error('Age must be a positive number');
-        };
-    } 
-  },
-  password: {
-      type: String,
-      required: true,
-      minlength: 7,
-      trim: true,
-      validate(value){
-          if (value.toLowerCase().includes('password')) {
-            throw Error('password can not contain "password"');
-          };
-      }
-  }  
-});
 
 // const me = new User({
 //     name: "lomkovsky",
@@ -66,9 +27,9 @@ const Task = mongoose.model('Task', {
         default: false
     }
 });
-const task = new Task({
-    description: "go to work   ",
-    completed: true
-});
-task.save().then(() => console.log("task saved ", task)).catch((e) => console.log("error!", e));
+// const task = new Task({
+//     description: "go to work   ",
+//     completed: true
+// });
+// task.save().then(() => console.log("task saved ", task)).catch((e) => console.log("error!", e));
 
