d57f917856f5cb232613d3af2bff03e8c37b75f7
lom <lomkovsky@gmail.com>
2019-08-20 11:26:28 +0300

    lesson 78 find and findOne methods added

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,16 +1,34 @@
 // CRUD create read update delete
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+const {MongoClient, ObjectID} = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
 MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client) => {
     if (error) {
-        return console.log('Unable to connect to database!')
-    }
+        return console.log('Unable to connect to database!');
+    };
     const db = client.db(databaseName);
+
+    db.collection('users').find({name: 'lom'}).toArray((error, users) => {
+        if (error) {
+             return console.log('can not find users');
+        };
+        console.log(users);
+    });
+
+    // db.collection('users').findOne({ _id: ObjectID("5d5ba6f0f5f19b0820fdc0dd"), }, (error, user) => { 
+    //     if (error) { 
+    //         return console.log('unable to fetch the user');
+    //     };
+    //     console.log(user);
+    // });
+
+
 
     // db.collection('users').insertOne({
     //     name: 'lom',
@@ -36,22 +54,22 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
     //     console.log(result.ops);
     // });
 
-    db.collection('buying').insertMany([{
-        description: 'lom',
-        completed: true
-    },
-    {
-        description: 'tania',
-        completed: false
-    },
-    {
-        description: 'bob',
-        completed: true
-    }], (error, result) => {
-        if (error) {
-           return colsole.log('Unable to insert documents');
-        }; 
-        console.log(result.ops);
-    });
+    // db.collection('buying').insertMany([{
+    //     description: 'lom',
+    //     completed: true
+    // },
+    // {
+    //     description: 'tania',
+    //     completed: false
+    // },
+    // {
+    //     description: 'bob',
+    //     completed: true
+    // }], (error, result) => {
+    //     if (error) {
+    //        return colsole.log('Unable to insert documents');
+    //     }; 
+    //     console.log(result.ops);
+    // });
 
 });
