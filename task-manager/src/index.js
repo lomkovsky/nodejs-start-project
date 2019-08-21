083417bf2fe40a9fa7378f77c4852615ce5fa9ba
lomkovsky <lomkovsky@gmail.com>
2019-08-21 18:22:29 +0300

    issue1 is solved

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -3,6 +3,7 @@ require('./db/mongoose');
 const User = require('./models/user.js');
 const Task = require('./models/task.js');
 const app = express();
+// const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -18,25 +19,36 @@ app.get('/users', (req, res) => {
 
 //read user by ID
 app.get('/users/:id', (req, res) => {
+   
+    // let objId = null;
+    // try {
+    //     objId = mongoose.Types.ObjectId(req.params.id);
+    // } catch (error) {
+    //     return res.status(400).json({error: `Not valid id ${req.params.id}`});
+    // }
+    
     const _id = req.params.id;
     User.findById(_id).then((user) => {
-        console.log(user);
         if (!user) {
-            console.log('USER NOT FOUND!2')
-            return res.status(404).send('USER NOT FOUND!2'); // 'USER NOT FOUND!'
+            return res.status(404).send('USER NOT FOUND!'); 
         }
-        console.log('USER FOUND');
         res.send(user);
     }).catch((e) => {
-        console.log('USER NOT FOUND!3')
-        res.status(500).send('USER NOT FOUND!3');
+        if(e.name === 'CastError') {
+            return res.status(400).json({error: `Not valid id ${req.params.id}`});
+        }
+        console.log(JSON.stringify(e, null, 2));
+        res.status(500).send();
     });
 });
+   
+
+
 // app.get('/users/:name', (req, res) => {
 //     const name = req.params.name;
 //     User.find({ name: name }).then((user) => {
 //         console.log(user);
-//         if (user.length < 1) {
+//         if (!user.length) {
 //             console.log('USER NOT FOUND!2')
 //             return res.status(404).send('USER NOT FOUND!2'); // 'USER NOT FOUND!'
 //         }
