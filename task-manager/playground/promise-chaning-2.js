6f62e93de185a0a823734b3eb75f2e70f77da3ee
lomkovsky <lomkovsky@gmail.com>
2019-08-22 17:52:30 +0300

    lesson 96 async await playground

--- task-manager/playground/promise-chaning-2.js
+++ task-manager/playground/promise-chaning-2.js
@@ -1,6 +1,19 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task.js');
-Task.findByIdAndDelete("5d5c1767d878152ec468cbb0")
-.then(() => Task.countDocuments({completed: true}))
-.then((result) => console.log(result))
-.catch((e) => console.log(e));
+// Task.findByIdAndDelete("5d5c1767d878152ec468cbb0")
+// .then(() => Task.countDocuments({completed: true}))
+// .then((result) => console.log(result))
+// .catch((e) => console.log(e));
+
+const deleteTaskByIdAndCount = async (id, completed) => {
+    const deluser = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({ completed });
+    if (!deluser){
+        console.log('No such tast found!');
+    };
+    return {deluser, count};
+};
+deleteTaskByIdAndCount("5d5c1783226d0e2ed69a503e", true).then((result) => {
+    console.log(result.deluser);
+    console.log(result.count);
+}).catch((e) => console.log(e));
--- task-manager/playground/promise-chaning.js
+++ task-manager/playground/promise-chaning.js
@@ -1,6 +1,18 @@
 require('../src/db/mongoose');
 const User = require('../src/models/user.js');
-User.findByIdAndUpdate("5d5c090217196228301c45d2", {age: 360}).then((user) => {
+// User.findByIdAndUpdate("5d5c090217196228301c45d2", {age: 360}).then((user) => {
+//     console.log(user);
+//     return User.countDocuments({ age: 360 });
+// }).then((countNumber) => console.log(countNumber)).catch((e) => console.log(e));
+
+const updateAgeAndCount = async (id, age) => {
+    try {
+    const user = await User.findByIdAndUpdate(id, { age });
+    const count = await User.countDocuments({ age });
     console.log(user);
-    return User.countDocuments({ age: 360 });
-}).then((countNumber) => console.log(countNumber)).catch((e) => console.log(e));
+    console.log(count);
+    } catch {
+        console.log(error);
+    };
+};
+updateAgeAndCount("5d5c090217196228301c45d2", 300);
