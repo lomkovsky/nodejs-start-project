ab1821337a604c32e04da15e732ce4cbd6e23a6d
lomkovsky <lomkovsky@gmail.com>
2019-08-23 19:05:32 +0300

    lesson 107 generating authentication token

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -10,6 +11,7 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -38,9 +40,40 @@ const userSchema = new mongoose.Schema({
         throw Error('password can not contain "password"');
       };
     }
-  }
+  },
+  tokens: [{
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
 
+//
+userSchema.methods.generateAuthToken = async function() {
+  const user = this;
+  const token = jwt.sign({ _id: user.id.toString() }, "thisisit");
+
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  
+  return token;
+}
+
+// find user by email and compare the hash and plain password
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error('Unable to login');
+  };
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error('Unable to login');
+  };
+  return user;
+};
+
+// hash the plain password before saving
 userSchema.pre('save', async function(next) {
   const user = this;
   if (user.isModified('password')) {
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -10,7 +10,22 @@ const User = require('../models/user.js');
       res.status(500).send(e);
     };
   });
-  
+  // 
+  router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        const token = await user.generateAuthToken();
+        // if (!user) {
+        //   res.status(400).send({error: "Unable to login"});
+        // }
+        res.send({ user, token });
+    } catch (e) {
+      res.status(400).send(e);
+    };
+  });
+
+
+
   //read user by ID
   router.get('/users/:id', async (req, res) => {
     try {
@@ -57,10 +72,10 @@ const User = require('../models/user.js');
   // create a new user
   router.post('/users', async (req, res) => {
     const user = new User(req.body);
-    console.log(user);
     try {
       await user.save();
-      res.status(201).send(user);
+      const token = await user.generateAuthToken();
+      res.status(201).send({ user, token });
     } catch (e) {
       res.status(400).send(e);
     };
